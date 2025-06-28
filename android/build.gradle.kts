c8089b9eef7e9e6d2afd876ab99b8f303c671006
Lucas Garcia Mendes <lucas.garcia@fteam.dev>
2025-06-28 16:43:27 -0300

    fix: Upgrade Kotlin version to 2.0.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
-    ext.kotlin_version = "1.9.22"
+    ext.kotlin_version = "2.0.0"
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     }
 }
 
